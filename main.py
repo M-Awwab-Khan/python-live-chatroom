3313be46ab5c44daf9d8779659c722735bf87cb9
Awwab <mawwabkhank2006@gmail.com>
2024-02-27 21:57:36 +0500

    form submit validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -9,6 +9,18 @@ socketio = SocketIO(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        code = request.form.get('code')
+        join = request.form.get('join', False)
+        create = request.form.get('create', False)
+
+        if not name:
+            return render_template('home.html', error='Please enter a name.')
+
+        if join and not code:
+            return render_template('home.html', error='Please enter a code.')
+
     return render_template('home.html')
 
 
