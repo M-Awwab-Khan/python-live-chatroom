0483ed4fa82fa80f7bf0f670947a81321bb9d142
Awwab <mawwabkhank2006@gmail.com>
2024-02-27 20:09:26 +0500

    template render

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -6,6 +6,11 @@ from string import ascii_uppercase
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hjhjsdahhds"
 socketio = SocketIO(app)
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    return render_template('home.html')
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
