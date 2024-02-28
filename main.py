6b7cdaba3cb0505a35c3c42b5f1f5ffe0cb95407
Awwab <mawwabkhank2006@gmail.com>
2024-02-28 19:52:04 +0500

    leave room logic

--- main.py
+++ main.py
@@ -79,5 +79,23 @@ def connect(auth):
     print(f"{name} has joined the room {room}.")
     
 
+@socketio.on('disconnect')
+def disconnect():
+    room = session.get('room')
+    name = session.get('name')
+
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]['members'] -= 1
+        if rooms[room]['members'] <= 0:
+            del rooms[room] 
+    
+    send({
+        "name": name,
+        "message": 'has left the room'
+    }, to=room)
+    print(f"{name} has left the room {room}.")
+
 if __name__ == "__main__":
     socketio.run(app, debug=True)
